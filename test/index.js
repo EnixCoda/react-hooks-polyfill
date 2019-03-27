da753a2d9cb43c8ddaff31b89c090e979285e9c5
Enix <enixcoda@gmail.com>
2019-03-27 17:26:27 +0800

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,60 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const { useEffect, useState, useContext, useHooks, holyShit } = require('../build/index')
+
+const TestContext = React.createContext(0)
+holyShit()
+const StateAndEffects = function WithState() {
+  let [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log('This run only ONCE')
+    // useState() // Un-comment this will raise an expected error.
+    const id = setInterval(() => setCount(++count), 1000)
+    return () => {
+      console.log(`Clearing interval`)
+      clearInterval(id)
+    }
+  }, [])
+  useEffect(() => {
+    console.log('This run every twice render')
+
+    return () => {
+      console.log(`Unsubscribing`)
+    }
+  }, [Math.round(count / 2)])
+  useEffect(() => {
+    console.log('This run at every render')
+    document.title = count
+  })
+  return (
+    <TestContext.Provider value={count}>
+      <button onClick={() => setCount(0)}>reset</button>
+      <ContextConsumer />
+    </TestContext.Provider>
+  )
+}
+
+const ContextConsumer = function ContextConsumer() {
+  const value = useContext(TestContext)
+  return value
+}
+
+const ToggleMount = function ToggleMount(props) {
+  const [active, setActive] = useState(true)
+  return (
+    <div>
+      <button onClick={() => setActive(!active)}>toggle</button>
+      {active ? props.children : null}
+    </div>
+  )
+}
+
+const root = document.querySelector('#react-root')
+if (root) {
+  ReactDOM.render(
+    <ToggleMount>
+      <StateAndEffects />
+    </ToggleMount>,
+    root
+  )
+}
